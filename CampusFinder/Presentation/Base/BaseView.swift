e3be64b06d22151cc2b6cef7654167c4c17f143c
rkdtnlzl <rkdtnlzl@naver.com>
2024-08-07 14:35:45 +0900

    [Feat]프로젝트 초기 설정

---
+++ CampusFinder/Presentation/Base/BaseView.swift
@@ -0,0 +1,29 @@
+//
+//  BaseView.swift
+//  CampusFinder
+//
+//  Created by 강석호 on 8/7/24.
+//
+
+import UIKit
+
+class BaseView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureConstraints()
+        configureUI()
+        configureAddTarget()
+    }
+    
+    func configureHierarchy() { }
+    func configureConstraints() { }
+    func configureUI() { }
+    func configureAddTarget() { }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ CampusFinder/Presentation/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingViewController.swift
+//  CampusFinder
+//
+//  Created by 강석호 on 8/7/24.
+//
+
+import UIKit
+
+final class OnboardingViewController: BaseViewController {
+    
+}
