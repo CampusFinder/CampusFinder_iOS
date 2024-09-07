df184379299c11898f781f42dfa30beb567b977a
rkdtnlzl <rkdtnlzl@naver.com>
2024-09-07 21:51:30 +0900

    [Feat/#1] CF 커스텀 버튼 추가

--- CampusFinder/Source/Util/CFButton.swift
+++ CampusFinder/Source/Util/CFButton.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class CFButton {
-    static func mainButton(title: String) -> UIButton {
+    static func primaryButton(title: String) -> UIButton {
         let button = UIButton(type: .system)
         button.setTitle(title, for: .normal)
-        button.setTitleColor(CFColor.black04, for: .normal)
-        button.backgroundColor = CFColor.Bg.gray03
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .pretendard(size: 16, weight: .bold)
+        button.backgroundColor = CFColor.Primary.blue01
         button.layer.cornerRadius = 10
+        return button
+    }
+    
+    static func writingButton(title: String) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        button.tintColor = CFColor.Primary.blue01
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(CFColor.Primary.blue01, for: .normal)
+        button.titleLabel?.font = .pretendard(size: 16, weight: .semibold)
+        button.backgroundColor = CFColor.Primary.blue04
+        button.layer.cornerRadius = 30
+        button.layer.borderWidth = 1
+        button.layer.borderColor = CFColor.Primary.blue03.cgColor
+        return button
+    }
+    
+    static func unselectedButton(title: String) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(CFColor.Primary.blue01, for: .normal)
+        button.titleLabel?.font = .pretendard(size: 16, weight: .semibold)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 1
+        button.layer.borderColor = CFColor.Primary.blue03.cgColor
         return button
     }
 }
