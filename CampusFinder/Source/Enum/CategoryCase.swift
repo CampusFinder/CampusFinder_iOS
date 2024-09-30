732a2a5d18fb198eed321758a2d19b8573d0da78
rkdtnlzl <rkdtnlzl@naver.com>
2024-09-30 21:33:51 +0900

    [Add] 카테고리별 Case Enum 분리

--- CampusFinder/Source/Enum/CategoryCase.swift
+++ CampusFinder/Source/Enum/CategoryCase.swift
@@ -2,40 +2,90 @@
 //  CategoryCase.swift
 //  CampusFinder
 //
-//  Created by 강석호 on 8/30/24.
+//  Created by 강석호 on 9/30/24.
 //
 
 import Foundation
 
 enum CategoryCase: String, CaseIterable {
-    case student = "학생찾기"
-    case request = "의뢰찾기"
+    case all = "전체"
+    case design = "디자인"
+    case document = "문서작성"
+    case develop = "개발"
+    case video = "영상"
+    case foreign = "외국어"
+    case ect = "기타"
     
-    var dummyData: [DummyData] {
+    var dummyData: [CategoryCaseDummyData] {
         switch self {
-        case .student:
+        case .all:
             return [
-                DummyData(title: "디자인 의뢰를 해드립니다 정말로 고수입니다ㅋㅋ", nickname: "화양리 보안관", isNearSchool: true),
-                DummyData(title: "강아지 사진 그려드립니다ㅎㅎ", nickname: "김선생", isNearSchool: true),
-                DummyData(title: "다양한 고양이 의뢰를 해드립니다", nickname: "젠지 화이팅", isNearSchool: false),
-                DummyData(title: "얼굴 그려드립니다 😥", nickname: "용원게이", isNearSchool: false),
-                DummyData(title: "다양한 디자인 의뢰를 해드립니다1111111", nickname: "흑백요리사", isNearSchool: false),
-                DummyData(title: "다양한 디자인 의뢰를 해드립니다222222", nickname: "안성재쉪", isNearSchool: true)
+                CategoryCaseDummyData(title: "디자인 의뢰를 해드립니다 정말로 고수입니다ㅋㅋ", nickname: "화양리 보안관", isNearSchool: true),
+                CategoryCaseDummyData(title: "강아지 사진 그려드립니다ㅎㅎ", nickname: "김선생", isNearSchool: true),
+                CategoryCaseDummyData(title: "다양한 고양이 의뢰를 해드립니다", nickname: "젠지 화이팅", isNearSchool: false),
+                CategoryCaseDummyData(title: "얼굴 그려드립니다 😥", nickname: "용원게이", isNearSchool: false),
+                CategoryCaseDummyData(title: "다양한 디자인 의뢰를 해드립니다1111111", nickname: "흑백요리사", isNearSchool: false),
+                CategoryCaseDummyData(title: "다양한 디자인 의뢰를 해드립니다222222", nickname: "안성재쉪", isNearSchool: true)
             ]
-        case .request:
+        case .design:
             return [
-                DummyData(title: "지금 학교에서 ppt 화면 제작해주실 분 구해요~!", nickname: "제시금액 1000000원", isNearSchool: true),
-                DummyData(title: "지금 학교에서 ppt 화면 제작해주실 분 구해요~!", nickname: "제시금액 1000000원", isNearSchool: true),
-                DummyData(title: "지금 학교에서 ppt 화면 제작해주실 분 구해요~!", nickname: "제시금액 1000000원", isNearSchool: false),
-                DummyData(title: "지금 학교에서 ppt 화면 제작해주실 분 구해요~!", nickname: "제시금액 1000000원", isNearSchool: false),
-                DummyData(title: "지금 학교에서 ppt 화면 제작해주실 분 구해요~!", nickname: "제시금액 1000000원", isNearSchool: true),
-                DummyData(title: "지금 학교에서 ppt 화면 제작해주실 분 구해요~!", nickname: "제시금액 1000000원", isNearSchool: true)
+                CategoryCaseDummyData(title: "디자인 의뢰를 해드립니다 정말로 고수입니다ㅋㅋ", nickname: "화양리 보안관", isNearSchool: true),
+                CategoryCaseDummyData(title: "강아지 사진 그려드립니다ㅎㅎ", nickname: "김선생", isNearSchool: true),
+                CategoryCaseDummyData(title: "다양한 고양이 의뢰를 해드립니다", nickname: "젠지 화이팅", isNearSchool: false),
+                CategoryCaseDummyData(title: "얼굴 그려드립니다 😥", nickname: "용원게이", isNearSchool: false),
+                CategoryCaseDummyData(title: "다양한 디자인 의뢰를 해드립니다1111111", nickname: "흑백요리사", isNearSchool: false),
+                CategoryCaseDummyData(title: "다양한 디자인 의뢰를 해드립니다222222", nickname: "안성재쉪", isNearSchool: true)
+            ]
+        case .document:
+            return [
+                CategoryCaseDummyData(title: "디자인 의뢰를 해드립니다 정말로 고수입니다ㅋㅋ", nickname: "화양리 보안관", isNearSchool: true),
+                CategoryCaseDummyData(title: "강아지 사진 그려드립니다ㅎㅎ", nickname: "김선생", isNearSchool: true),
+                CategoryCaseDummyData(title: "다양한 고양이 의뢰를 해드립니다", nickname: "젠지 화이팅", isNearSchool: false),
+                CategoryCaseDummyData(title: "얼굴 그려드립니다 😥", nickname: "용원게이", isNearSchool: false),
+                CategoryCaseDummyData(title: "다양한 디자인 의뢰를 해드립니다1111111", nickname: "흑백요리사", isNearSchool: false),
+                CategoryCaseDummyData(title: "다양한 디자인 의뢰를 해드립니다222222", nickname: "안성재쉪", isNearSchool: true)
+            ]
+        case .develop:
+            return [
+                CategoryCaseDummyData(title: "디자인 의뢰를 해드립니다 정말로 고수입니다ㅋㅋ", nickname: "화양리 보안관", isNearSchool: true),
+                CategoryCaseDummyData(title: "강아지 사진 그려드립니다ㅎㅎ", nickname: "김선생", isNearSchool: true),
+                CategoryCaseDummyData(title: "다양한 고양이 의뢰를 해드립니다", nickname: "젠지 화이팅", isNearSchool: false),
+                CategoryCaseDummyData(title: "얼굴 그려드립니다 😥", nickname: "용원게이", isNearSchool: false),
+                CategoryCaseDummyData(title: "다양한 디자인 의뢰를 해드립니다1111111", nickname: "흑백요리사", isNearSchool: false),
+                CategoryCaseDummyData(title: "다양한 디자인 의뢰를 해드립니다222222", nickname: "안성재쉪", isNearSchool: true)
+            ]
+        case .video:
+            return [
+                CategoryCaseDummyData(title: "디자인 의뢰를 해드립니다 정말로 고수입니다ㅋㅋ", nickname: "화양리 보안관", isNearSchool: true),
+                CategoryCaseDummyData(title: "강아지 사진 그려드립니다ㅎㅎ", nickname: "김선생", isNearSchool: true),
+                CategoryCaseDummyData(title: "다양한 고양이 의뢰를 해드립니다", nickname: "젠지 화이팅", isNearSchool: false),
+                CategoryCaseDummyData(title: "얼굴 그려드립니다 😥", nickname: "용원게이", isNearSchool: false),
+                CategoryCaseDummyData(title: "다양한 디자인 의뢰를 해드립니다1111111", nickname: "흑백요리사", isNearSchool: false),
+                CategoryCaseDummyData(title: "다양한 디자인 의뢰를 해드립니다222222", nickname: "안성재쉪", isNearSchool: true)
+            ]
+        case .foreign:
+            return [
+                CategoryCaseDummyData(title: "디자인 의뢰를 해드립니다 정말로 고수입니다ㅋㅋ", nickname: "화양리 보안관", isNearSchool: true),
+                CategoryCaseDummyData(title: "강아지 사진 그려드립니다ㅎㅎ", nickname: "김선생", isNearSchool: true),
+                CategoryCaseDummyData(title: "다양한 고양이 의뢰를 해드립니다", nickname: "젠지 화이팅", isNearSchool: false),
+                CategoryCaseDummyData(title: "얼굴 그려드립니다 😥", nickname: "용원게이", isNearSchool: false),
+                CategoryCaseDummyData(title: "다양한 디자인 의뢰를 해드립니다1111111", nickname: "흑백요리사", isNearSchool: false),
+                CategoryCaseDummyData(title: "다양한 디자인 의뢰를 해드립니다222222", nickname: "안성재쉪", isNearSchool: true)
+            ]
+        case .ect:
+            return [
+                CategoryCaseDummyData(title: "디자인 의뢰를 해드립니다 정말로 고수입니다ㅋㅋ", nickname: "화양리 보안관", isNearSchool: true),
+                CategoryCaseDummyData(title: "강아지 사진 그려드립니다ㅎㅎ", nickname: "김선생", isNearSchool: true),
+                CategoryCaseDummyData(title: "다양한 고양이 의뢰를 해드립니다", nickname: "젠지 화이팅", isNearSchool: false),
+                CategoryCaseDummyData(title: "얼굴 그려드립니다 😥", nickname: "용원게이", isNearSchool: false),
+                CategoryCaseDummyData(title: "다양한 디자인 의뢰를 해드립니다1111111", nickname: "흑백요리사", isNearSchool: false),
+                CategoryCaseDummyData(title: "다양한 디자인 의뢰를 해드립니다222222", nickname: "안성재쉪", isNearSchool: true)
             ]
         }
     }
 }
 
-struct DummyData {
+struct CategoryCaseDummyData {
     let title: String
     let nickname: String
     let isNearSchool: Bool
