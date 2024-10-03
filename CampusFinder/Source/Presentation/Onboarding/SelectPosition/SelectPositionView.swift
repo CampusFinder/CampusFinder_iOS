f029cce0307d2f17790fd9f83fc48cea0e10894b
rkdtnlzl <rkdtnlzl@naver.com>
2024-10-03 21:08:02 +0900

    [Feat] 직위 선택 뷰 UI 구현

--- CampusFinder/Source/Presentation/Onboarding/SelectPosition/SelectPositionView.swift
+++ CampusFinder/Source/Presentation/Onboarding/SelectPosition/SelectPositionView.swift
@@ -10,28 +10,70 @@ import SnapKit
 
 final class SelectPositionView: BaseView {
     
-    let termsLabel = UILabel()
-    let nextButton = CFButton.primaryButton(title: "다음")
+    let introLabel = UILabel()
+    let detailLabel = UILabel()
+    let selectStudentView = CFSelectRoleView()
+    let selectStudentButton = UIButton()
+    let selectProfessorView = CFSelectRoleView()
+    let selectProfessorButton = UIButton()
     
     override func configureHierarchy() {
-        addSubview(termsLabel)
-        addSubview(nextButton)
+        addSubview(introLabel)
+        addSubview(detailLabel)
+        addSubview(selectStudentView)
+        addSubview(selectStudentButton)
+        addSubview(selectProfessorView)
+        addSubview(selectProfessorButton)
     }
     
     override func configureUI() {
-        termsLabel.text = "교수 학생 선택 화면"
-        termsLabel.textColor = .black
-        termsLabel.font = .systemFont(ofSize: 15)
+        introLabel.text = "만나서 반가워요!\n캠파를 시작해볼까요?"
+        introLabel.textColor = .black
+        introLabel.font = .pretendard(size: 26, weight: .bold)
+        introLabel.textAlignment = .center
+        introLabel.numberOfLines = 2
+        
+        detailLabel.text = "함께하시는 분의 [직위]를 알려주세요"
+        detailLabel.textColor = CFColor.black03
+        detailLabel.font = .pretendard(size: 16, weight: .medium)
+        detailLabel.textAlignment = .center
+        detailLabel.numberOfLines = 1
+        
+        selectStudentView.selectedRole = "학생"
+        selectStudentView.configureUI()
+        
+        selectStudentButton.backgroundColor = .clear
+        
+        selectProfessorView.selectedRole = "교수"
+        selectProfessorView.configureUI()
+        
+        selectProfessorButton.backgroundColor = .clear
     }
     
     override func configureConstraints() {
-        termsLabel.snp.makeConstraints { make in
-            make.center.equalToSuperview()
+        introLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(190)
+            make.centerX.equalToSuperview()
         }
-        nextButton.snp.makeConstraints { make in
-            make.bottom.equalToSuperview().inset(60)
-            make.horizontalEdges.equalToSuperview().inset(30)
-            make.height.equalTo(44)
+        detailLabel.snp.makeConstraints { make in
+            make.top.equalTo(introLabel.snp.bottom).offset(14)
+            make.centerX.equalToSuperview()
+        }
+        selectStudentView.snp.makeConstraints { make in
+            make.top.equalTo(detailLabel.snp.bottom).offset(110)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalTo(150)
+        }
+        selectStudentButton.snp.makeConstraints { make in
+            make.edges.equalTo(selectStudentView)
+        }
+        selectProfessorView.snp.makeConstraints { make in
+            make.top.equalTo(selectStudentView.snp.bottom).offset(14)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalTo(150)
+        }
+        selectProfessorButton.snp.makeConstraints { make in
+            make.edges.equalTo(selectProfessorView)
         }
     }
 }
