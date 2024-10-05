628f1bfc0715b5a758def6efd4eb613041f9c8cd
rkdtnlzl <rkdtnlzl@naver.com>
2024-10-05 16:01:27 +0900

    [Feat] 회원가입 싱글톤 Model 관리

---
+++ CampusFinder/Source/Model/Onboarding/SignupData.swift
@@ -0,0 +1,30 @@
+//
+//  SignupData.swift
+//  CampusFinder
+//
+//  Created by 강석호 on 10/4/24.
+//
+
+import Foundation
+
+class SignupData {
+    static let shared = SignupData()
+    
+    private init() {}
+
+    var role: String?
+    var nickname: String?
+    var password: String?
+    var phoneNumber: String?
+    var univName: String?
+    var email: String?
+    
+    func reset() {
+        role = nil
+        nickname = nil
+        password = nil
+        phoneNumber = nil
+        univName = nil
+        email = nil
+    }
+}
