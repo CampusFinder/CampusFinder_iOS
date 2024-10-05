04aa87a0037cb2942e971e20f960e77c02cbb60c
rkdtnlzl <rkdtnlzl@naver.com>
2024-10-05 15:19:13 +0900

    [Add] 회원가입 DTO 추가

---
+++ CampusFinder/Source/Network/Onboarding/RequestDTO/SignUpQuery.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpQuery.swift
+//  CampusFinder
+//
+//  Created by 강석호 on 10/4/24.
+//
+
+import Foundation
+
+// MARK: 회원가입
+struct SignUpQuery: Encodable {
+    let role: String
+    let email: String
+    let phoneNumber: String
+    let password: String
+    let nickname: String
+    let univName: String
+}
